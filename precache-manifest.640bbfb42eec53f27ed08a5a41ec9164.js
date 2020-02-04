6a138943b1bca1fd0bd46ca3855c741f93bff0c8
atib shaikh <atibshaikh@gmail.com>
2020-02-04 08:14:19 +0530

    Updates

---
+++ precache-manifest.640bbfb42eec53f27ed08a5a41ec9164.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46b7a2ec77305dbc270ef3cdaa03ed90",
+    "url": "/monster-app/index.html"
+  },
+  {
+    "revision": "41937465e376c0ebe294",
+    "url": "/monster-app/static/css/main.ae13be08.chunk.css"
+  },
+  {
+    "revision": "b6b4b7653795c37734cb",
+    "url": "/monster-app/static/js/2.81bb5c18.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-app/static/js/2.81bb5c18.chunk.js.LICENSE"
+  },
+  {
+    "revision": "41937465e376c0ebe294",
+    "url": "/monster-app/static/js/main.04ebd319.chunk.js"
+  },
+  {
+    "revision": "5e9313ab051bd47c454a",
+    "url": "/monster-app/static/js/runtime-main.465a8a10.js"
+  }
+]);
